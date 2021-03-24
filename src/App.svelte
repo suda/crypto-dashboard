92026b2fa192b4933510e5a583e3ca80771ae06b
Wojtek Siudzinski <admin@suda.pl>
2021-03-24 21:37:37 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Chart from './components/Chart.svelte';
+</script>
+
+<main class="bg-gray-200 grid grid-cols-2 grid-rows-2 gap-6 p-6">
+	<Chart class="m-6" />
+	<Chart fsym="BTC" />
+	<Chart fsym="LTC" />
+	<Chart fsym="XLM" />
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+	main {
+		font-family: 'IBM Plex Mono', sans-serif;
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ src/components/Chart.svelte
@@ -0,0 +1,135 @@
+<script>
+  import { onMount } from "svelte";
+  import { createChart } from "lightweight-charts";
+  import cc from "cryptocompare";
+  import _ from "lodash";
+
+  let chartElement, chart, candlestickSerie, volumeSerie, pricesData, volumeData;
+  let change = 0;
+  let lastPrice = 0;
+  let totalVolume = 0;
+
+  export let fsym = "ETH";
+  export let tsym = "EUR";
+  export let timeframe = "Minute"; // Also possible Hour and Day
+
+  const donationAddresses = {
+    "BTC": "1K7Uc9MDzokCGEsUxrQQXn59VQ1Q9AWGvZ",
+    "BCH": "1BHM8oACVN6F4xksXAAP3nR9EoAkfd8sGF",
+    "ETH": "0x8CcA7589d8061ad4D3D913412d1EAD71cbEae081",
+    "LTC": "LRJ4Evzmyynp8Uswiy7e2uWhPKBNiSHroq",
+    "NU": "0x921B265a6DB3C3b0e3DD9CBD7aD9634B52a0EbdB",
+    "BAT": "0x52680f27fC20A60AbEe9413128b4420eC86709C4",
+    "ADA": "addr1vye3t0y88qvhuyf3png0xthncyggjcxhtdflc7cavh3fkjqw7u868",
+    "UNI": "0xd7884711a4Db2d1392Eafd2d5109de49B019E76F",
+    "LINK": "0x72E3Ad2332Ae59FE09Bfe2fAce3a3F5E71ee9B7e"
+  };
+
+  const pricesMapFn = (d) =>
+    _.pick(d, ["time", "open", "high", "low", "close"]);
+  const volumeMapFn = ({ volumeto, time }) => {
+    return {
+      value: volumeto,
+      time
+    }
+  };
+
+  const getData = async () => {
+    const data = await cc[`histo${timeframe}`](fsym, tsym);
+    const prices = data.map(pricesMapFn);
+    const volume = data.map(volumeMapFn);
+    lastPrice = prices[prices.length - 1].close;
+    change = lastPrice / prices[0].open * 100 - 100;
+    totalVolume = volume.reduce((previous, current) => {
+      return previous + current.value;
+    }, 0);
+    return { prices, volume };
+  };
+
+  const initCandlestick = (chart, data) => {
+    const serie = chart.addCandlestickSeries({
+      upColor: "rgb(38,166,154)",
+      downColor: "rgb(255,82,82)",
+      wickUpColor: "rgb(38,166,154)",
+      wickDownColor: "rgb(255,82,82)",
+      borderVisible: false,
+    });
+    serie.setData(data);
+    return serie;
+  };
+
+  const initVolume = (chart, data) => {
+    const serie = chart.addHistogramSeries({
+      color: "rgba(0, 0, 0, 0.1)",
+      priceFormat: {
+        type: "volume",
+      },
+      priceScaleId: "",
+      scaleMargins: {
+        top: 0.8,
+        bottom: 0,
+      },
+    });
+    serie.setData(data);
+    return serie;
+  };
+
+  onMount(async () => {
+    const data = await getData();
+    pricesData = data.prices;
+    volumeData = data.volume;
+
+    chart = createChart(chartElement, {
+      width: chartElement.width,
+      height: chartElement.height,
+      timeScale: {
+        timeVisible: timeframe !== 'Day'
+      },
+      layout: {
+        fontFamily: 'IBM Plex Mono'
+      }
+    });
+    candlestickSerie = initCandlestick(chart, pricesData);
+    volumeSerie = initVolume(chart, volumeData);
+  });
+</script>
+
+<style>
+  #chart-container {
+    height: 100%;
+    width: 100%;
+    @apply shadow p-6 rounded-lg bg-white;
+  }
+  #chart {
+    height: calc(100% - 4.5rem);
+    width: 100%;
+  }
+</style>
+
+<div id="chart-container">
+  <div class="flex justify-between items-center h-12 pb-3">
+    <div>
+      <div class="text-xl md:text-sm text-gray-800 font-bold leading-tight">{fsym}-{tsym}</div>
+      <div class="text-gray-400 md:text-xs">Market</div>
+    </div>
+    <div>
+      <div class="text-xl md:text-sm text-gray-800 font-bold leading-tight">{lastPrice.toLocaleString()} {tsym}</div>
+      <div class="text-gray-400 md:text-xs">Last trade price</div>
+    </div>
+    <div>
+      <div class="text-xl md:text-sm text-gray-800 font-bold leading-tight {change > 0 ? 'text-green-500' : 'text-red-500'}">{change.toFixed(2)}%</div>
+      <div class="text-gray-400 md:text-xs">24h price</div>
+    </div>
+    <div>
+      <div class="text-xl md:text-sm text-gray-800 font-bold leading-tight">{totalVolume.toLocaleString(undefined, { minimumFractionDigits: 2 })} {fsym}</div>
+      <div class="text-gray-400 md:text-xs">24h volume</div>
+    </div>
+  </div>
+  <div id="chart" bind:this={chartElement} />
+  {#if donationAddresses[fsym]}
+  <div class="bg-purple-400 leading-none text-white rounded-lg p-2 shadow text-xs w-full">
+    Support this work by donating <strong>{fsym}</strong> to:
+    <strong>{donationAddresses[fsym]}</strong>
+  </div>
+  {/if}
+</div>
