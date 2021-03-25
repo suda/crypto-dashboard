89e476d8e59aaefc0c6b77b6b7221a61086bc29b
Wojtek Siudzinski <admin@suda.pl>
2021-03-25 17:39:40 +0100

    Add a dummy chart component

---
+++ src/components/DummyChart.svelte
@@ -0,0 +1,12 @@
+<div id="chart-container">
+  <h1 class="text-9xl">+</h1>
+  <p class="text-5xl">Add chart</p>
+</div>
+
+<style>
+  #chart-container {
+    height: 100%;
+    width: 100%;
+    @apply p-6 rounded-lg border-8 border-dashed border-gray-300 text-gray-400 flex flex-col justify-center items-center;
+  }
+</style>
