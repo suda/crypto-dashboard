bde27b9037cb05284687e1174be041e24389e058
Wojtek Siudzinski <admin@suda.pl>
2021-03-26 11:40:12 +0100

    Keep the charts in store

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,40 @@
 <script>
-	import Chart from './components/Chart.svelte';
+	import NavBar from "./components/NavBar.svelte";
+	import Chart from "./components/Chart.svelte";
+	import DummyChart from "./components/DummyChart.svelte";
+
+	import { charts, removeOne } from "./store";
+
+	const remove = ({ fsym, tsym }) => {
+		removeOne({ fsym, tsym });
+	};
 </script>
 
-<main class="bg-gray-200 grid grid-cols-2 grid-rows-2 gap-6 p-6">
-	<Chart />
-	<Chart fsym="BTC" />
-	<Chart fsym="LTC" />
-	<Chart fsym="XLM" />
+<main class="bg-gray-200 p-6">
+	<NavBar />
+	<article class="grid grid-cols-2 grid-rows-2 gap-6">
+		{#if !$charts.length}
+			<DummyChart />
+		{/if}
+
+		{#each $charts as chart}
+			<Chart removeCallback={remove} fsym={chart.fsym} tsym={chart.tsym} />
+		{/each}
+	</article>
 </main>
 
 <style global lang="postcss">
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 
 	main {
-		font-family: 'IBM Plex Mono', sans-serif;
+		font-family: "IBM Plex Mono", sans-serif;
 		width: 100%;
 		height: 100%;
+	}
+	article {
+		width: 100%;
+		height: calc(100% - 5.5rem);
 	}
 </style>
---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { writable } from 'svelte/store';
+
+const stored = localStorage.charts ? JSON.parse(localStorage.charts) : undefined;
+let lastValue = stored;
+export const charts = writable(stored || [{ fsym: 'BTC', tsym: 'USD' }]);
+charts.subscribe((value) => {
+  localStorage.charts = JSON.stringify(value);
+  lastValue = value;
+});
+export const removeOne = ({ fsym, tsym }) => {
+  lastValue = lastValue.filter((o) => !(o.fsym === fsym && o.tsym === tsym));
+  charts.set(lastValue);
+};
