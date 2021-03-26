8d14b680ddaab3bae8f886b76868535f96389afb
Wojtek Siudzinski <admin@suda.pl>
2021-03-26 22:02:31 +0100

    Refactor the common dialog functions

--- src/components/AddChartDialog.svelte
+++ src/components/AddChartDialog.svelte
@@ -1,6 +1,6 @@
 <script>
+  import Dialog from "./Dialog.svelte";
   import Select from "./Select.svelte";
-  import Button from "./Button.svelte";
   import { tsyms, fsyms } from "../dataSource";
 
   let fsym;
@@ -14,28 +14,8 @@
   };
 </script>
 
-<div
-  class="{visible
-    ? ''
-    : 'hidden'} absolute inset-0 flex justify-center items-center z-10"
->
-  <div
-    class="bg-gradient-to-b from-blue-100 to-blue-400 absolute inset-0 z-20"
-    on:click={onCancel}
-  />
-  <div class="bg-white rounded-lg z-30">
-    <div class="w-96 border-t-8 border-blue-600 rounded-lg flex">
-      <div class="w-full pt-4 px-4">
-        <h3 class="font-bold text-blue-700">Add a chart</h3>
-        <p class="py-4 text-sm text-gray-400">Select the market:</p>
-        <Select options={fsyms} onChange={(o) => (fsym = o)} />
-        <Select options={tsyms} onChange={(o) => (tsym = o)} />
-      </div>
-    </div>
-
-    <div class="p-4 flex space-x-4">
-      <Button onClick={onCancel}>Cancel</Button>
-      <Button type="primary" onClick={onAdd}>Add</Button>
-    </div>
-  </div>
-</div>
+<Dialog title="Add a chart" positiveButtonLabel="Add" onCancel={onCancel} onSubmit={onAdd} visible={visible}>
+  <p class="py-4 text-sm text-gray-400">Select the market:</p>
+  <Select options={fsyms} onChange={(o) => (fsym = o)} />
+  <Select options={tsyms} onChange={(o) => (tsym = o)} />
+</Dialog>
---
+++ src/components/Dialog.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Button from "./Button.svelte";
+
+  export let visible = false;
+  export let title = "Dialog";
+  export let positiveButtonLabel = "OK";
+  export let nonCancelable = false;
+  export let onCancel = () => {};
+  export let onSubmit = () => {};
+</script>
+
+<div
+  class="{visible
+    ? ''
+    : 'hidden'} absolute inset-0 flex justify-center items-center z-10"
+>
+  <div
+    class="bg-gradient-to-b from-blue-100 to-blue-400 absolute inset-0 z-20"
+    on:click={onCancel}
+  />
+  <div class="bg-white rounded-lg z-30">
+    <div class="w-96 border-t-8 border-blue-600 rounded-lg flex">
+      <div class="w-full pt-4 px-4">
+        <h3 class="font-bold text-blue-700">{title}</h3>
+        <slot></slot>
+      </div>
+    </div>
+
+    <div class="p-4 flex space-x-4">
+      {#if !nonCancelable}
+        <Button onClick={onCancel}>Cancel</Button>
+      {/if}
+      <Button type="primary" onClick={onSubmit}>{positiveButtonLabel}</Button>
+    </div>
+  </div>
+</div>
