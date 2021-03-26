0f2f459a84238b3d9c346a81665fa8e30976bf07
Wojtek Siudzinski <admin@suda.pl>
2021-03-26 11:44:27 +0100

    Keep the last updated live

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,16 +1,29 @@
 <script>
-
+  export let lastUpdated = new Date();
 </script>
 
 <nav>
   <div>
     <h1 class="text-base font-bold">Crypto Dashboard</h1>
-    <h2 class="text-xs text-gray-400">Last updated: Thu 25 Mar 20:02</h2>
+    <h2 class="text-xs text-gray-400">
+      Last updated: {lastUpdated.toLocaleString()}
+    </h2>
   </div>
   <div>
     <div class="relative inline-flex">
-      <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
-      <select class="border border-gray-300 rounded-lg text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
+      <svg
+        class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 412 232"
+        ><path
+          d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
+          fill="#648299"
+          fill-rule="nonzero"
+        /></svg
+      >
+      <select
+        class="border border-gray-300 rounded-lg text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none"
+      >
         <option value="1">1m</option>
         <option value="5">5m</option>
         <option value="15">15m</option>
@@ -19,7 +32,9 @@
         <option value="1440">1d</option>
       </select>
     </div>
-    <button class="border border-blue-300 rounded-lg text-white h-10 pl-5 pr-5 bg-blue-400 hover:border-blue-500 focus:outline-none appearance-none">
+    <button
+      class="border border-blue-300 rounded-lg text-white h-10 pl-5 pr-5 bg-blue-400 hover:border-blue-500 focus:outline-none appearance-none"
+    >
       + Add a chart
     </button>
   </div>
